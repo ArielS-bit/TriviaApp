c00d3bd625cc7d4f5c6832ba0723d2af2fff4523
Ariel Shoham <ariel.shoham400@gmail.com>
2021-05-14 01:29:29 +0300

    WelcomePage including style

--- TriviaXamarinApp/TriviaXamarinApp/Views/WelcomePage.xaml.cs
+++ TriviaXamarinApp/TriviaXamarinApp/Views/WelcomePage.xaml.cs
@@ -8,22 +8,32 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using TriviaXamarinApp.ViewModel;
 
+
 namespace TriviaXamarinApp.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class WelcomePage : ContentPage
     {
+        ConsoleColor colorBtn;
         public WelcomePage()
         {
             InitializeComponent();
             BindingContext = new WelcomePageViewModel();
-            logo.Source = "https://images-platform.99static.com/ToOcJbuDzoa8sqctL1QMGO089kA=/200x220:1800x1820/500x500/top/smart/99designs-contests-attachments/95/95660/attachment_95660693";
-
+            
+            //Image
+            //logo.Source = "ttps://images-platform.99static.com/ToOcJbuDzoa8sqctL1QMGO089kA=/200x220:1800x1820/500x500/top/smart/99designs-contests-attachments/95/95660/attachment_95660693";
+            
+            //Button
+            btn.CornerRadius = 14;
+            btn.Margin = 85;
+            btn.HeightRequest = 45;
+            colorBtn = ConsoleColor.Green;
+        
         }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-
+            //btn.BackgroundColor = colorBtn;
         }
     }
 }
